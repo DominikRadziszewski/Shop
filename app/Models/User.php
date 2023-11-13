2c5ac732e8dfefc88101b50db7f7af5453a1028a
DominikRadziszewski <dominik.m.radziszewski@gmail.com>
2023-11-13 21:38:21 +0100

    Migrtion file and expand registration

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,8 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'surname',
+        'phone_number',
         'email',
         'password',
     ];
---
+++ database/migrations/2023_11_13_201217_add_phone_number_and_surname_columns_to_users_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('surname')->after('name')->nullable();
+            $table->string('phone_number', 20)->after('surname')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('surname','phone_number');
+        });
+    }
+};
