213b76cdbe349868b132f14547012adfb0a4454d
DominikRadziszewski <dominik.m.radziszewski@gmail.com>
2023-11-14 22:55:02 +0100

    Pagination Added to users list

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,7 +14,7 @@ class UserController extends Controller
     public function index()
     {
         return view("users.index" , [
-            "users"=> User::all()
+            "users"=> User::paginate(3)
         ]);
     }
 
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Providers;
+use Illuminate\Pagination\Paginator;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -19,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+     Paginator::useBootstrapFive();
+     Paginator::useBootstrapFour();
     }
 }
