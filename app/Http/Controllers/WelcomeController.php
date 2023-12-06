23a2270622afdc2b1e192b768e8852631e5b5934
DominikRadziszewski <dominik.m.radziszewski@gmail.com>
2023-12-07 00:28:03 +0100

    Debugging

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -20,7 +20,6 @@ class WelcomeController extends Controller
         $paginate = $request->query('paginate') ?? 5;
     
         $query= Product::query();
-        $query->paginate($paginate);
         if (!is_null($filters)) {
     
             if (array_key_exists('categories',$filters)) {
@@ -43,16 +42,12 @@ class WelcomeController extends Controller
     
             }
     
-            return response()->json([
-    
-                'data'=> $query->get()
-    
-            ]);
+            return response()->json($query->paginate($paginate));
     
         }
     
         return view("welcome",
-        ["products"=> $query->get(),
+        ["products"=>         $query->paginate($paginate),
         "categories"=>ProductCategory::orderBy("name", "asc")->get(),
         "defaultImage"=>"https://via.placeholder.com/240x240/5fa9f8/efefef"
         ]);
--- resources/js/welcome.js
+++ resources/js/welcome.js
@@ -8,7 +8,7 @@ $(function()
 
     $('a#filter-button').on("click", function(event){
         event.preventDefault();
-        getProducts($('a.products-actual-count').text());
+        getProducts($('a.products-actual-count').first().text());
      });
 
         function getProducts(paginate){
