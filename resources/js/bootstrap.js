3872f7ceb79490b568999a92f778124b9547074c
DominikRadziszewski <dominik.m.radziszewski@gmail.com>
2023-12-21 18:07:03 +0100

    Csrf

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,6 +10,11 @@ window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+window.$.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf_token"]').attr('content')
+    }
+});
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
--- resources/js/delete.js
+++ resources/js/delete.js
@@ -1,38 +1,40 @@
-$(function(){
-    $(".delete").on("click",function(){ 
+$(function () {
+  $(".delete").on("click", function () {
       Swal.fire({
-        title: confirmDelete,
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Tak",
-        cancelButtonText:"Nie"
-                }).then((result) => {
-      if (result.isConfirmed) {
-        $.ajax({
-        method: "delete",
-        url: deleteUrl+$(this).data("id")
-                })
-        .then(
-          function(response) {
-            location.reload();
-                            },
-        function(jqXHR, textStatus, errorThrown) {
-          var responseJSON = JSON.parse(jqXHR.responseText);
-          console.log(responseJSON.message);
-          Swal.fire({
-        title: responseJSON.status,
-          text: responseJSON.message,
-          icon: "error",
-                  });
-                                                }
-              );
+          title: confirmDelete,
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Tak",
+          cancelButtonText: "Nie"
+      }).then((result) => {
+          if (result.isConfirmed) {
+              // Pobierz CSRF token z tagu meta
+              var csrfToken = $('meta[name="csrf-token"]').attr('content');
 
-      
-                            };
-    
-                                    });
-      
+              // Dodaj CSRF token do nagłówków żądania AJAX
+              $.ajax({
+                  method: "delete",
+                  url: deleteUrl + $(this).data("id"),
+                  headers: {
+                      'X-CSRF-TOKEN': csrfToken
+                  }
+              }).then(
+                  function (response) {
+                      location.reload();
+                  },
+                  function (jqXHR, textStatus, errorThrown) {
+                      var responseJSON = JSON.parse(jqXHR.responseText);
+                      console.log(responseJSON.message);
+                      Swal.fire({
+                          title: responseJSON.status,
+                          text: responseJSON.message,
+                          icon: "error",
+                      });
+                  }
+              );
+          }
+      });
   });
 });
